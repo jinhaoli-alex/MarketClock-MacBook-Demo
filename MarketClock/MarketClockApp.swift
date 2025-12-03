c4a1cd5df4f5f9d7cf38af472b99e401064f1f47
Jinhao Li <z5603183@ad.unsw.edu.au>
2025-12-03 18:25:28 +1100

    Initial Commit

---
+++ MarketClock/MarketClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketClockApp.swift
+//  MarketClock
+//
+//  Created by AL on 3/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarketClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
